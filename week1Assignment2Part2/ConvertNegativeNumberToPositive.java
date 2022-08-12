5f07a3404915e5a9c9fd28ab2862aea94fe0bed8
mail2Sangeetha <110739983+mail2Sangeetha@users.noreply.github.com>
2022-08-12 20:55:05 +0530

    Add files via upload
    ConvertNegativeNumberToPositive

---
+++ week1Assignment2Part2/ConvertNegativeNumberToPositive.java
@@ -0,0 +1,23 @@
+package week1Assignment2Part2;
+
+public class ConvertNegativeNumberToPositive {
+	
+	public static void main(String[] args) {
+		int Number = -4;
+		int Multiply=-1;
+		int Convert;
+		if(Number<0) {
+			 Convert= Number*Multiply;
+			System.out.println("Negative value converted into Positive value = " +Convert  );	
+		}
+		else {
+			System.out.println("Negative value converted Not into Positive value" );
+		}
+			
+			
+			
+		
+			
+	}
+
+}
